5dfe6a6c608f153f86ec849f23ab2921bc7ca735
Matthias Kestenholz <mk@feinheit.ch>
2018-10-02 08:17:58 +0200

    Modernize the URLconf

--- feincms_banners/urls.py
+++ feincms_banners/urls.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import url, patterns
+from django.conf.urls import url
+
+from feincms_banners import views
 
 
-urlpatterns = patterns(
-    'feincms_banners.views',
-    url(r'^b/c/(?P<code>[^/]+)/$', 'click', name='banner_click'),
-    url(r'^b/i/(?P<code>[^/]+)/$', 'impression', name='banner_impression'),
-)
+urlpatterns = [
+    url(r'^b/c/(?P<code>[^/]+)/$', views.click, name='banner_click'),
+    url(r'^b/i/(?P<code>[^/]+)/$', views.impression, name='banner_impression'),
+]
